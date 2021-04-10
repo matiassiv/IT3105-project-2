f3aa78d3ec2596b429e3c0d1d5a9cda4edd3613f
Matias <sivertsvikm@gmail.com>
2021-04-10 22:14:22 +0200

    Add tournament of progressive policies

--- topp.py
+++ topp.py
@@ -23,13 +23,79 @@ class TOPP:
             model.eval()
             self.contenders.append(model)
 
-    
+    def create_tournament_matchings(self):
+        """
+        Returns an array of tuples with indices to the contender list
+        representing the matchings for the tournament
+        """
+        matchings = []
+        for i in range(self.num_contenders):
+            for j in range(i):
+                matchings.append((i, j))
+        
+        return matchings
+
+    def play_tournament(self):
+        matchings = self.create_tournament_matchings()
+
+        # Create a dictionary to hold the tournament results
+        results = {}
+        for i in range(self.num_contenders):
+            results[i] = [0 for i in range(self.num_contenders)]
+        
+        for match in matchings:
+            model_1 = self.contenders[match[0]]
+            model_2 = self.contenders[match[1]]
+            m1_results, m2_results = self.play_match(model_1, model_2)
+
+            if m1_results > m2_results:
+                results[match[0]][match[1]] = 1
+                results[match[1]][match[0]] = -1
+            elif m2_results > m1_results:
+                results[match[1]][match[0]] = 1
+                results[match[0]][match[1]] = -1
+            # If match is drawn, then the initialised 0 remains
+        return results
+
     def play_match(self, model_1, model_2):
-        for i in range(self.num_games):
-            pass
+        m1_results = 0
+        m2_results = 0
+        halfway_point = self.num_games//2
+        # Model 1 starts all matches in first half
+        for i in range(halfway_point):
+            # Alternate colors to start to test more of the network
+            starting_color = i % 2 + 1
+            result = self.play_game(model_1, model_2, starting_color)
+
+            # Model 1 is starting player, so if starting color wins then model 1 wins
+            if result == starting_color:
+                m1_results += 1
+                #m1_results.append(1)
+                #m2_results.append(-1)
+            else:
+                m2_results += 1
+                #m1_results.append(-1)
+                #m2_results.append(1)
+        
+        for i in range(halfway_point):
+            # Alternate colors to start to test more of the network
+            starting_color = i % 2 + 1
+            result = self.play_game(model_2, model_1, starting_color)
+
+            # Model 2 is starting player, so if starting color wins then model 2 wins
+            if result == starting_color:
+                m2_results += 1
+                #m1_results.append(-1)
+                #m2_results.append(1)
+            else:
+                m1_results += 1
+                #m1_results.append(1)
+                #m2_results.append(-1)
+        
+        return m1_results, m2_results
     
-    def play_game(self, p1, p2, starting_player=1):
-        game = StateManager(starting_player)
+    def play_game(self, p1, p2, starting_color=1):
+        game = StateManager(starting_color)
         m1 = MCTS(game, p1, 0)
         m2 = MCTS(game, p2, 0)
         s = game.get_game_state()
@@ -47,3 +113,14 @@ class TOPP:
             result = game.check_game_ended(s)
             if result:
                 return result
+
+    def print_tournament_results(self, results):
+
+        total_scores = [sum(results[i]) for i in range(self.num_contenders)]
+        
+        for i in range(self.num_contenders):
+            print(f"Contender {i}")
+            print("Results:", results[i])
+            print("Total score:", total_scores[i])
+            print("-----------------------------------------------")
+        
