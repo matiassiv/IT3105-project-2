c7e674c8592161ac530d8cff98b171607eeb518b
Matias Sivertsvik <matiassi@stud.ntnu.no>
2021-04-11 10:41:31 +0200

    Topp works as intended

--- topp.py
+++ topp.py
@@ -1,8 +1,10 @@
 import torch
+import numpy as np
 import config as cfg
 from state_manager import StateManager
 from mcts import MCTS
 from NN_architectures.hex_ann import HexANN
+
 
 class TOPP:
     def __init__(self, num_contenders, num_games, contender_paths):
@@ -13,7 +15,7 @@ class TOPP:
         game = StateManager()
         input_size = game.get_game_size()
         output_size = len(game.generate_legal_moves(game.get_game_state()))
-        
+
         # Create a list of contender models
         self.contenders = []
 
@@ -23,6 +25,8 @@ class TOPP:
             model.eval()
             self.contenders.append(model)
 
+        print(self.contenders)
+
     def create_tournament_matchings(self):
         """
         Returns an array of tuples with indices to the contender list
@@ -30,9 +34,9 @@ class TOPP:
         """
         matchings = []
         for i in range(self.num_contenders):
-            for j in range(i):
+            for j in range(i+1, self.num_contenders):
                 matchings.append((i, j))
-        
+
         return matchings
 
     def play_tournament(self):
@@ -42,12 +46,13 @@ class TOPP:
         results = {}
         for i in range(self.num_contenders):
             results[i] = [0 for i in range(self.num_contenders)]
-        
+
         for match in matchings:
+            print(match, flush=True)
             model_1 = self.contenders[match[0]]
             model_2 = self.contenders[match[1]]
             m1_results, m2_results = self.play_match(model_1, model_2)
-
+            print(f"M1: {m1_results} M2: {m2_results}")
             if m1_results > m2_results:
                 results[match[0]][match[1]] = 1
                 results[match[1]][match[0]] = -1
@@ -70,13 +75,13 @@ class TOPP:
             # Model 1 is starting player, so if starting color wins then model 1 wins
             if result == starting_color:
                 m1_results += 1
-                #m1_results.append(1)
-                #m2_results.append(-1)
+                # m1_results.append(1)
+                # m2_results.append(-1)
             else:
                 m2_results += 1
-                #m1_results.append(-1)
-                #m2_results.append(1)
-        
+                # m1_results.append(-1)
+                # m2_results.append(1)
+
         for i in range(halfway_point):
             # Alternate colors to start to test more of the network
             starting_color = i % 2 + 1
@@ -85,15 +90,15 @@ class TOPP:
             # Model 2 is starting player, so if starting color wins then model 2 wins
             if result == starting_color:
                 m2_results += 1
-                #m1_results.append(-1)
-                #m2_results.append(1)
+                # m1_results.append(-1)
+                # m2_results.append(1)
             else:
                 m1_results += 1
-                #m1_results.append(1)
-                #m2_results.append(-1)
-        
+                # m1_results.append(1)
+                # m2_results.append(-1)
+
         return m1_results, m2_results
-    
+
     def play_game(self, p1, p2, starting_color=1):
         game = StateManager(starting_color)
         m1 = MCTS(game, p1, 0)
@@ -101,13 +106,13 @@ class TOPP:
         s = game.get_game_state()
         while True:
             # TODO add actual game update for potential display of game
-            action = m1.getActionProb(s)
+            action = np.argmax(m1.getActionProb(s))
             a = game.one_hot_to_action(action)
             s = game.generate_next_state(s, a)
             result = game.check_game_ended(s)
             if result:
                 return result
-            action = m2.getActionProb(s)
+            action = np.argmax(m2.getActionProb(s))
             a = game.one_hot_to_action(action)
             s = game.generate_next_state(s, a)
             result = game.check_game_ended(s)
@@ -117,10 +122,18 @@ class TOPP:
     def print_tournament_results(self, results):
 
         total_scores = [sum(results[i]) for i in range(self.num_contenders)]
-        
+
         for i in range(self.num_contenders):
             print(f"Contender {i}")
             print("Results:", results[i])
             print("Total score:", total_scores[i])
             print("-----------------------------------------------")
-        
+
+
+if __name__ == "__main__":
+    path = "trained_models/hex_5/iteration_"
+    tournament = TOPP(
+        4, 14, [path+"0.pt", path+"50.pt", path+"150.pt", path+"350.pt"])
+
+    results = tournament.play_tournament()
+    tournament.print_tournament_results(results)
